205eb650d2383b7ce66cdaa307f54285b23dca7f
bradleycfleming <brad10fleming@gmail.com>
2025-04-12 18:01:51 -0500

    Outfit algo and autocomplete city

--- app/components/Outfit.js
+++ app/components/Outfit.js
@@ -1,34 +1,136 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Outfit = (props) => {
+  const [top, setTop] = useState("T-shirt");
+  const [bottom, setBottom] = useState("Shorts");
+  const [second, setSecond] = useState(null);
+  const [outerwear, setOuterwear] = useState(null);
+  const [hat, setHat] = useState(null);
+
+  const onUpdateOutfit = () => {
+    setSecond(null);
+    setOuterwear(null);
+    setHat(null);
+
+    switch (props.formality) {
+      // casual
+      case "1":
+        setTop("T-shirt");
+        setBottom("Shorts");
+        if (props.temp < 75) {
+          if (props.temp < 60) {
+            setSecond("Hoodie");
+            setBottom("Sweatpants");
+          } else {
+            setSecond("Flannel");
+          }
+        }
+        break;
+
+      // casual - daily
+      case "2":
+        setTop("T-shirt");
+        setBottom("Shorts");
+        if (props.temp < 75) {
+          if (props.temp < 60) {
+            setSecond("Hoodie");
+            setBottom("Jeans");
+          } else {
+            setSecond("Flannel");
+          }
+        }
+        break;
+
+      // daily
+      case "3":
+        setTop("Uniqlo shirt");
+        setBottom("Shorts");
+        if (props.temp < 75) {
+          if (props.temp < 60) {
+            setSecond("Sweater");
+            setBottom("Jeans");
+          } else {
+            setSecond("Flannel");
+          }
+        }
+        break;
+
+      // daily - smart
+      case "4":
+        setTop("Uniqlo shirt");
+        setBottom("Khaki shorts");
+        if (props.temp < 75) {
+          if (props.temp < 60) {
+            setSecond("Sweater");
+            setBottom("Chinos");
+          } else {
+            setSecond("Corduroy shirt");
+          }
+        }
+        break;
+
+      // smart
+      case "5":
+        setTop("Collared shirt");
+        setBottom("Chinos");
+        if (props.temp < 60) {
+          setSecond("Sweater");
+        }
+        break;
+    }
+
+    // determine if a coat is necessary
+    if (props.temp < 40) {
+      setOuterwear("Coat");
+    } else {
+      // determine if a rainjacket is necessary
+      if (props?.rain && props.rain["1h"] > 0.1) {
+        setOuterwear("Rain jacket");
+      }
+    }
+
+    // determine if a hat is necessary
+    if (parseInt(props.formality) < 4) {
+      if (props.temp < 40) {
+        setHat("Beanie");
+      } else {
+        if (props?.wind && props.wind?.gust > 20) {
+          setHat("Baseball cap");
+        }
+      }
+    }
+  };
+
+  useEffect(() => {
+    onUpdateOutfit();
+  }, [props.formality, props.temp]);
+
   return (
     <div id={props.id} className={props.className}>
       <table className="table">
-        {/* head */}
         <thead></thead>
         <tbody>
-          {/* row 1 */}
           <tr>
             <th>Top</th>
-            <td>T-shirt</td>
+            <td>{top}</td>
           </tr>
-          {/* row 2 */}
           <tr>
             <th>Bottom</th>
-            <td>Shorts</td>
+            <td>{bottom}</td>
           </tr>
-          <TableLine present={props.formality>2} row="2nd" info="Flannel"></TableLine>
+          <TableLine
+            present={second !== null}
+            row="2nd"
+            info={second}
+          ></TableLine>
 
-          {/* row 3 */}
-          <tr>
-            <th>Shoes</th>
-            <td>Sneakers</td>
-          </tr>
+          <TableLine
+            present={outerwear !== null}
+            row="Outerwear"
+            info={outerwear}
+          ></TableLine>
 
-
-
-          <TableLine present="true" row="Formality" info={props.formality}></TableLine>
-
+          <TableLine present={hat !== null} row="Hat" info={hat}></TableLine>
         </tbody>
       </table>
     </div>
--- app/page.js
+++ app/page.js
@@ -4,10 +4,67 @@ import Outfit from "./components/Outfit";
 import { useState, useRef, useEffect } from "react";
 
 export default function Home() {
-  const [formality, setFormality] = useState(3);
+  const [formality, setFormality] = useState("3");
+  const [city, setCity] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  const [temp, setTemp] = useState(null);
+  const [wind, setWind] = useState(null);
+  const [rain, setRain] = useState(null);
 
   const handleFormalityChange = (e) => {
     setFormality(e.target.value);
+  };
+
+  const handleCityChange = async (e) => {
+    const inputValue = e.target.value;
+    setCity(inputValue);
+
+    if (inputValue.length > 1) {
+      // console.log(e.target.value)
+
+      const citySuggestions = await getCityLocation(inputValue);
+
+      if (citySuggestions.length > 0) {
+        setSuggestions(citySuggestions);
+        setShowSuggestions(true);
+      } else {
+        setShowSuggestions(false);
+      }
+    } else {
+      setShowSuggestions(false);
+    }
+  };
+
+  const handleSuggestionClick = (suggestion) => {
+    const city = suggestion;
+    setCity(city.name);
+    setShowSuggestions(false);
+
+    getWeatherByLocation(city.lat, city.lon, setTemp, setWind, setRain);
+  };
+
+  const success = (position) => {
+    const { latitude, longitude } = position.coords;
+    console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+
+    // Deselect the input
+    document.getElementById("cityInput").blur();
+
+    getWeatherByLocation(latitude, longitude, setTemp, setWind, setRain);
+  };
+
+  const error = () => {
+    alert("Sorry, no position available.");
+  };
+
+  const getLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, error);
+      setCity("Your location");
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
   };
 
   return (
@@ -17,46 +74,66 @@ export default function Home() {
           <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-center mb-8 text-primary">
             Weather Wardrobe
           </h1>
-          <div className="w-full flex flex-col items-center mb-8">
-              <label className="input input-md">
-                <input
-                  type="text"
-                  placeholder="Where are you located? " 
-                  className="grow text-base"
-                />
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="w-6 h-6 inline-block ml-2 hover:text-primary hover:cursor-pointer transition"
-                  onClick={getLocation}
-                >
-                  <path d="M12 2C8.686 2 6 4.686 6 8c0 4.5 6 12 6 12s6-7.5 6-12c0-3.314-2.686-6-6-6zm0 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4z" />
-                </svg>
-              </label>
-
+          <div className="w-full flex flex-col items-center mb-8 relative">
+            <label className="input input-md w-full">
               <input
-                type="range"
-                id="formality"
-                min={1}
-                max={5}
-                value={formality}
-                onChange={handleFormalityChange}
-                className="range range-xs md:range-sm range-primary mt-8"
-                step={1}
+                id="cityInput"
+                type="text"
+                placeholder="What is your city? "
+                className="grow text-base "
+                value={city}
+                onChange={handleCityChange}
               />
-              <div className="w-full max-w-xs flex justify-between px-2 mt-3 text-sm">
-                <span>Casual</span>
-                <span>Daily</span>
-                <span>Smart</span>
-              </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-6 h-6 inline-block ml-2 hover:text-primary hover:cursor-pointer transition"
+                onClick={getLocation}
+              >
+                <path d="M12 2C8.686 2 6 4.686 6 8c0 4.5 6 12 6 12s6-7.5 6-12c0-3.314-2.686-6-6-6zm0 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4z" />
+              </svg>
+            </label>
+
+            {showSuggestions && suggestions.length > 0 && (
+              <ul className="absolute top-full left-0 w-full max-w-md bg-base-100 rounded-md shadow-md z-10">
+                {suggestions.map((suggestion, index) => (
+                  <li
+                    key={index}
+                    className="px-4 py-2 hover:bg-base-300 cursor-pointer"
+                    onClick={() => handleSuggestionClick(suggestion)}
+                  >
+                    {suggestion.name}, {suggestion.state}, {suggestion.country}
+                  </li>
+                ))}
+              </ul>
+            )}
           </div>
 
-          {/* <button className="btn btn-primary m-8 " onClick={getLocation}>
-            Get Outfit
-          </button> */}
+          <input
+            type="range"
+            id="formality"
+            min={1}
+            max={5}
+            value={formality}
+            onChange={handleFormalityChange}
+            className="range range-sm range-primary w-full max-w-md mb-3"
+            step={1}
+          />
+          <div className="w-full  flex justify-between px-2 max-w-md mb-8 text-sm">
+            <span>Casual</span>
+            <span>Daily</span>
+            <span>Smart</span>
+          </div>
 
-          <Outfit id="outfit" className="hidden rounded-box " formality={formality}></Outfit>
+          <Outfit
+            id="outfit"
+            className="hidden rounded-box "
+            formality={formality}
+            temp={temp}
+            wind={wind}
+            rain={rain}
+          ></Outfit>
 
           <div id="display" className="stats stats-primary shadow hidden mt-5">
             <div className="stat">
@@ -80,16 +157,24 @@ export default function Home() {
   );
 }
 
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(success, error);
-  } else {
-    document.getElementById("data").innerHTML =
-      "Geolocation is not supported by this browser.";
+async function getCityLocation(city) {
+  const apiKey = process.env.NEXT_PUBLIC_OPENWEATHER_API_KEY;
+  const url = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}&limit=5`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Failed to fetch city location");
+    }
+
+    const cityLocation = await response.json();
+    return cityLocation;
+  } catch (error) {
+    alert(`Error fetching city location: ${error.message}`);
   }
 }
 
-async function getWeatherByLocation(lat, lon) {
+async function getWeatherByLocation(lat, lon, setTemp, setWind, setRain) {
   const apiKey = process.env.NEXT_PUBLIC_OPENWEATHER_API_KEY;
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
 
@@ -101,32 +186,27 @@ async function getWeatherByLocation(lat, lon) {
 
     // Weather
     const weatherData = await response.json();
+
+    console.log(weatherData.wind);
+    console.log(weatherData.rain);
+
+    setTemp(weatherData.main.feels_like);
+    setWind(weatherData.wind);
+    setRain(weatherData.rain);
     displayResults(weatherData);
   } catch (error) {
-    document.getElementById(
-      "data"
-    ).innerHTML = `Error fetching weather data: ${error.message}`;
+    alert(`Error fetching weather data: ${error.message}`);
   }
 }
 
 function displayResults(weatherData) {
-  document.getElementById("display").classList.remove("hidden");
-  document.getElementById("outfit").classList.remove("hidden");
-
   const tempElement = document.getElementById("temp");
   const descriptionElement = document.getElementById("description");
   const iconElement = document.getElementById("weatherIcon");
   tempElement.innerHTML = `${weatherData.main.temp} °F`;
   descriptionElement.innerHTML = `${weatherData.weather[0].main}`;
   iconElement.src = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
-}
 
-function success(position) {
-  const { latitude, longitude } = position.coords;
-  console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
-  getWeatherByLocation(latitude, longitude);
-}
-
-function error() {
-  alert("Sorry, no position available.");
+  document.getElementById("display").classList.remove("hidden");
+  document.getElementById("outfit").classList.remove("hidden");
 }
